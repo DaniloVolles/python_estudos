0330b48480c9a0e04db90147cd4be0249b27206a
danilo_volles_araujo <danilo.volles@outlook.com>
2023-05-24 13:09:50 -0300

    utilização de métodos externos na ccodificação.

---
+++ teoria/metodosDifDiretorios/main1.py
@@ -0,0 +1,14 @@
+from teoria.metodosDifDiretorios.function.metodo1 import soma
+# Para isso funcionar o programa deve estar organizado em pacotes.
+# funcionou.
+
+print("--------------------")
+print("Execução do programa")
+print("--------------------")
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+soma = soma(n1, n2)
+
+print(f"A soma entre {n1} e {n2} é {soma}.")
