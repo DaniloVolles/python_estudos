b3b6a0d46cc9520ba17ce44a5f75b656cb850c9d
danilo_volles_araujo <danilo.volles@outlook.com>
2023-06-12 12:34:44 -0300

    jellyfish_aplicacao organização para exemplos.

--- teoria/manipulacao_string/jellyfish/jellyfish_aplicacao.py
+++ teoria/manipulacao_string/jellyfish/jellyfish_aplicacao.py
@@ -1,14 +1,18 @@
 import jellyfish as jf
 
-string = input("Digite a string: ")
-comparacao = 'Conceição do Pará'
+comparacao = 'Três Corações'
+string = input("Digite a string: ").strip()
 
 levenshtein = jf.levenshtein_distance(string, comparacao)
 damerau_levenshtein = jf.damerau_levenshtein_distance(string, comparacao)
 jaro = jf.jaro_similarity(string, comparacao)
 jaro_winkler = jf.jaro_winkler_similarity(string, comparacao, True)
 
-print('Tamanho da String: ', len(string))
+print('----------------------------------------------')
+print('- String:', string)
+print('- Comparação:', comparacao)
+print('- Tamanho da String:', len(string))
+print('----------------------------------------------')
 
 print('Distância de Levenshtein:', levenshtein)
 print('Distância de Damerau-Levenshtein:', damerau_levenshtein)
